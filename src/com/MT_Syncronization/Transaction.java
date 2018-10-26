6942d9611d3308870651b16971791f176d8b652d
Administrator <amitp499@gmail.com>
2018-10-26 13:18:23 +0530

    Core Java - Day5

---
+++ src/com/MT_Syncronization/Transaction.java
@@ -0,0 +1,24 @@
+package com.MT_Syncronization;
+
+public class Transaction implements Runnable{
+
+    private Customer customer;
+    private float amount;
+
+
+
+    public Transaction(Customer customer, float amount) {
+        super();
+        this.customer = customer;
+        this.amount = amount;
+    }
+
+
+
+    @Override
+    public void run() {
+        String name = Thread.currentThread().getName();
+        System.out.println( name+" Performing withdrawal operation ");
+        customer.withdraw(amount);
+    }
+}
